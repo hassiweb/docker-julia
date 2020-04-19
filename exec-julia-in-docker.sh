a4632d14a5afddc3c46149350f35d0acaf5dac18
hassiweb <nryk.hashimoto@gmail.com>
2020-04-19 12:36:25 +0900

    Initial commit

---
+++ exec-julia-in-docker.sh
@@ -0,0 +1,2 @@
+docker-compose exec julia julia
+
---
+++ julia/packages.jl
@@ -0,0 +1,7 @@
+using Pkg
+
+Pkg.add("ArgParse")
+Pkg.add(PackageSpec(url="https://github.com/staticfloat/InfluxDB.jl", rev="master"))
+
+# Run `julia packages.jl` to install packages above
+
