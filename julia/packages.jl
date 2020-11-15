7bdc20d2567346c5996fb8e4e19b3f45d8ac7922
hassiweb <nryk.hashimoto@gmail.com>
2020-11-16 02:18:40 +0900

    Update Julia version of 1.5.2

--- julia/packages.jl
+++ julia/packages.jl
@@ -1,7 +1,8 @@
 using Pkg
 
-Pkg.add("ArgParse")
-Pkg.add(PackageSpec(url="https://github.com/staticfloat/InfluxDB.jl", rev="master"))
+# Add julia packages to install like comments below when building the container image
+#Pkg.add("ArgParse")
+#Pkg.add(PackageSpec(url="https://github.com/staticfloat/InfluxDB.jl", rev="master"))
+
 
 # Run `julia packages.jl` to install packages above
-
